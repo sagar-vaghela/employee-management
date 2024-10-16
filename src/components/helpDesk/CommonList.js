f70d324852e25994cc60155c1c763c95fdddae2d
Sagar Vaghela <sagar19vaghela.sv@gmail.com>
2024-10-16 16:09:02 +0530

    Add Drag and Drop

--- src/components/helpDesk/CommonList.js
+++ src/components/helpDesk/CommonList.js
@@ -11,16 +11,25 @@ function CommonList(props) {
     title,
     placeholder,
   } = props;
+
+  const moveitem = (dragIndex, hoverIndex) => {
+    const draggedTask = fields[dragIndex];
+    const updatedList = [...fields];
+    updatedList.splice(dragIndex, 1);
+    updatedList.splice(hoverIndex, 0, draggedTask);
+    setFields(updatedList);
+  };
   return (
     <>
-      <div className="form-group stud_detail">
-        <label htmlFor="client_name" className="task_label">
+      <div className='form-group stud_detail'>
+        <label htmlFor='client_name' className='task_label'>
           {title}
         </label>
         {fields.map((field, index) => {
           return (
             <Notes
               key={field.id}
+              moveitem={moveitem}
               setFields={setFields}
               fields={fields}
               index={index}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,15 @@
 import "@/styles/globals.css";
 import "../styles/bootstrap.min.css";
 import "@fortawesome/fontawesome-svg-core";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
+      <DndProvider backend={HTML5Backend}>
+        <Component {...pageProps} />
+      </DndProvider>
     </>
   );
 }
